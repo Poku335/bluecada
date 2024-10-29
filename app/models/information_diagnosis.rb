51ce3be2ec06dc9b331b6bd280c558af79394b8f
PakornChaenglew <pakornkp092meen@gmail.com>
2024-10-29 10:32:26 +0700

    fix: set update

--- app/models/information_diagnosis.rb
+++ app/models/information_diagnosis.rb
@@ -1,12 +1,12 @@
 class InformationDiagnosis < ApplicationRecord
 
-  attr_accessor :case_type_id
+  # attr_accessor :case_type_id
 
-  before_update :update_case_type
+  # before_update :update_case_type
 
-  def update_case_type
-    cancer_form = CancerForm.find_by(information_diagnosis_id: self.id) if self.id.present?
-    cancer_form.cancer_information.update(case_type_id: self.case_type_id) if cancer_form.present?
-  end
+  # def update_case_type
+  #   cancer_form = CancerForm.find_by(information_diagnosis_id: self.id) if self.id.present?
+  #   cancer_form.cancer_information.update(case_type_id: self.case_type_id) if cancer_form.present?
+  # end
 
 end
